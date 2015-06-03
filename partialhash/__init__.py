934c41a9218922d93ea4c91fe65197b8c5ea1f81
F483 <fabian.barkhau@gmail.com>
2015-06-03 22:10:10 +0200

    added scatter

--- partialhash/__init__.py
+++ partialhash/__init__.py
@@ -5,54 +5,59 @@
 
 from __future__ import print_function
 from __future__ import unicode_literals
-import sys
 import hashlib
 import codecs
 
 
-class PartialHash(object):
+class BoundsError(Exception):
 
-    class BoundsError(Exception):
-
-        def __init__(self, size, length, offset):
-            msg = ("Bounds error! length (%s), offset (%s) must be >= 0"
-                   " and length + offset (%s) <= size (%s).")
-            args = (length, offset, length + offset, size)
-            Exception.__init__(self, msg % args)
-
-    def bytestoint(self, data):
-        return int(codecs.encode(data, 'hex'), 16)
-
-    def file_obj(self, obj, offset=0, length=0, hash_algorithm=hashlib.sha256):
-        hasher = hash_algorithm()
-
-        # bounds check
-        obj.seek(0,2)
-        size = obj.tell()
-        if length < 0 or offset < 0 or length + offset > size:
-            raise self.BoundsError(size, length, offset)
-
-        # start reading from offset
-        obj.seek(offset)
-
-        # hash data
-        buf = obj.read(length) if length else obj.read()
-        hasher.update(buf)
-        digest = hasher.hexdigest()
-
-        return digest
-
-        # TODO nextseed = digest
-        # TODO nextoffset = self.bytestoint(digest) % (size - lenght)
-
-    def file_path(self, path, *args, **kwargs):
-        with open(path, 'rb') as obj:
-            return self.file_obj(obj, *args, **kwargs)
-
-    def __call__(self, f, *args, **kwargs):
-        if type(f) in [type('str'), type(u'unicode')]:
-            return self.file_path(f, *args, **kwargs)
-        return self.file_obj(f, *args, **kwargs)
+    def __init__(self, size, length, offset):
+        msg = ("Bounds error! length (%s), offset (%s) must be >= 0"
+               " and length + offset (%s) <= size (%s).")
+        args = (length, offset, length + offset, size)
+        Exception.__init__(self, msg % args)
 
 
-sys.modules[__name__] = PartialHash()
+def bytestoint(data):
+    return int(codecs.encode(data, 'hex'), 16)
+
+
+def file_obj(obj, offset=0, length=0, seed=b"", scatter=0,
+             hash_algorithm=hashlib.sha256):
+
+    # bounds check
+    obj.seek(0, 2)
+    size = obj.tell()
+    if length < 0 or offset < 0 or length + offset > size:
+        raise BoundsError(size, length, offset)
+
+    # start reading from offset
+    obj.seek(offset)
+
+    # add seed if given
+    hasher = hash_algorithm()
+    if seed:
+        hasher.update(seed)
+
+    # hash data
+    buf = obj.read(length) if length else obj.read()
+    hasher.update(buf)
+    digest = hasher.digest()
+
+    # scatter if given
+    if scatter:
+        offset = bytestoint(digest) % (size - length)
+        return file_obj(obj, offset=offset, length=length, seed=digest,
+                        scatter=(scatter - 1), hash_algorithm=hash_algorithm)
+    return digest
+
+
+def file_path(path, *args, **kwargs):
+    with open(path, 'rb') as obj:
+        return file_obj(obj, *args, **kwargs)
+
+
+def sum(f, *args, **kwargs):
+    if type(f) in [type('str'), type(u'unicode')]:
+        return file_path(f, *args, **kwargs)
+    return file_obj(f, *args, **kwargs)
--- tests/basic.py
+++ tests/basic.py
@@ -6,6 +6,7 @@
 
 from __future__ import print_function
 from __future__ import unicode_literals
+import binascii
 import json
 import unittest
 import partialhash
@@ -14,56 +15,87 @@ import partialhash
 fixtures = json.load(open("tests/fixtures.json"))
 
 
-class TestPartialHash(unittest.TestCase):
+def h2b(h):
+    return binascii.unhexlify(h.encode("ascii"))
 
-    def test_fullfile(self):
+
+class TestHash(unittest.TestCase):
+
+    def test_full(self):
         path = fixtures["full"]["path"]
-        expected = fixtures["full"]["sha256"]
-        digest = partialhash(path)
+        expected = h2b(fixtures["full"]["sha256"])
+        digest = partialhash.sum(path)
         self.assertEqual(digest, expected)
 
-    def test_offset(self):
-        path = fixtures["offset"]["path"]
-        offset = fixtures["offset"]["offset"]
-        expected = fixtures["offset"]["sha256"]
-        digest = partialhash(path, offset=offset)
+    def test_partial(self):
+        path = fixtures["partial"]["path"]
+        length = fixtures["partial"]["length"]
+        offset = fixtures["partial"]["offset"]
+        expected = h2b(fixtures["partial"]["sha256"])
+        digest = partialhash.sum(path, length=length, offset=offset)
         self.assertEqual(digest, expected)
+
+    def test_bounds(self):
+        def callback():
+            path = fixtures["bounds"]["path"]
+            length = fixtures["bounds"]["length"]
+            partialhash.sum(path, length=length)
+        self.assertRaises(partialhash.BoundsError, callback)
+
+
+class TestLength(unittest.TestCase):
 
     def test_length(self):
         path = fixtures["length"]["path"]
         length = fixtures["length"]["length"]
-        expected = fixtures["length"]["sha256"]
-        digest = partialhash(path, length=length)
+        expected = h2b(fixtures["length"]["sha256"])
+        digest = partialhash.sum(path, length=length)
         self.assertEqual(digest, expected)
-
-    def test_bounds_exceeded(self):
-        def callback():
-            path = fixtures["bounds_exceeded"]["path"]
-            length = fixtures["bounds_exceeded"]["length"]
-            partialhash(path, length=length)
-        self.assertRaises(partialhash.BoundsError, callback)
 
     def test_negative_length(self):
         def callback():
             path = fixtures["negative_length"]["path"]
             length = fixtures["negative_length"]["length"]
-            partialhash(path, length=length)
+            partialhash.sum(path, length=length)
         self.assertRaises(partialhash.BoundsError, callback)
+
+
+class TestOffset(unittest.TestCase):
+
+    def test_offset(self):
+        path = fixtures["offset"]["path"]
+        offset = fixtures["offset"]["offset"]
+        expected = h2b(fixtures["offset"]["sha256"])
+        digest = partialhash.sum(path, offset=offset)
+        self.assertEqual(digest, expected)
 
     def test_negative_offset(self):
         def callback():
             path = fixtures["negative_offset"]["path"]
             offset = fixtures["negative_offset"]["offset"]
-            partialhash(path, offset=offset)
+            partialhash.sum(path, offset=offset)
         self.assertRaises(partialhash.BoundsError, callback)
 
-    def test_offset_length(self):
-        path = fixtures["offset_length"]["path"]
-        length = fixtures["offset_length"]["length"]
-        offset = fixtures["offset_length"]["offset"]
-        expected = fixtures["offset_length"]["sha256"]
-        digest = partialhash(path, length=length, offset=offset)
-        self.assertEqual(digest, expected)
+
+class TestSeed(unittest.TestCase):
+
+    def test_seed(self):
+        seed_path = fixtures["seed"]["seed_path"]
+        file_path = fixtures["seed"]["file_path"]
+        expected = h2b(fixtures["seed"]["sha256"])
+        with open(seed_path, 'rb') as seed:
+            digest = partialhash.sum(file_path, seed=seed.read())
+            self.assertEqual(digest, expected)
+
+
+class TestScatter(unittest.TestCase):
+
+    def test_scatter(self):
+        path = fixtures["scatter"]["path"]
+        length = fixtures["scatter"]["length"]
+        offset = fixtures["scatter"]["offset"]
+        partialhash.sum(path, length=length, offset=offset, scatter=4)
+        # FIXME how to test correctness?
 
 
 if __name__ == '__main__':
