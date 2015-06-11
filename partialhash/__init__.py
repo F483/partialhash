f90a85585f819e994cd704601eda51d4b052e033
F483 <fabian.barkhau@gmail.com>
2015-06-11 20:29:41 +0200

    initial sample implementation

--- partialhash/__init__.py
+++ partialhash/__init__.py
@@ -6,6 +6,7 @@
 from __future__ import print_function
 from __future__ import unicode_literals
 import hashlib
+import codecs
 
 
 class BoundsError(Exception):
@@ -17,12 +18,29 @@ class BoundsError(Exception):
         Exception.__init__(self, msg % args)
 
 
+def _file_size(obj):
+    obj.seek(0, 2)
+    return obj.tell()
+
+
+def _as_chunks(length, limit):
+    return [limit] * (length // limit) + [length % limit]
+
+
+def _is_filepath(path):
+    return type(path) in [type(b'bytes'), type('str'), type(u'unicode')]
+
+
+def _bytes_to_int(data):
+    return int(codecs.encode(data, 'hex_codec'), 16)
+
+
 def compute_from_obj(obj, offset=0, length=0, seed=b"",
                      hash_algorithm=hashlib.sha256):
 
-    # bounds check
-    obj.seek(0, 2)
-    size = obj.tell()
+    # check input
+    assert(isinstance(seed, bytes))
+    size = _file_size(obj)
     length = length if length else size - offset
     if length < 0 or offset < 0 or length + offset > size:
         raise BoundsError(size, length, offset)
@@ -36,12 +54,42 @@ def compute_from_obj(obj, offset=0, length=0, seed=b"",
         hasher.update(seed)
 
     # hash data
-    chunklimit = 1024 * 1024 * 100  # 100mb
-    chunks = [chunklimit] * (length // chunklimit) + [length % chunklimit]
-    for chunksize in chunks:
-        buf = obj.read(chunksize)
+    chunks = _as_chunks(length, 1024 * 1024 * 100)  # max 100mb chunks
+    for chunk_size in chunks:
+        buf = obj.read(chunk_size)
         hasher.update(buf)
     return hasher.digest()
+
+
+def sample_from_obj(obj, sample_size, sample_count, seed=b"",
+                    hash_algorithm=hashlib.sha256):
+
+    # check input
+    assert(sample_size > 0 and sample_count > 0)
+    assert(isinstance(seed, bytes))
+
+    # split into chunks of sample_size or less
+    # always at least on chunk, possibly of size 0
+    chunks = _as_chunks(_file_size(obj), sample_size)
+
+    # hash chunks from pool and remove already hashed to spread evenly
+    chunks_pool = list(enumerate(chunks[:]))  # enumerate to remember position
+    for index_not_used in range(sample_count):
+
+        # refill poll when empty
+        if len(chunks_pool) == 0:
+            chunks_pool = list(enumerate(chunks[:]))
+
+        # hash chunk
+        chunk_index = _bytes_to_int(seed) % len(chunks_pool)  # index from seed
+        chunk_position, length = chunks_pool[chunk_index]
+        offset = chunk_position * sample_size
+        digest = compute_from_obj(obj, offset=offset, length=length, seed=seed,
+                                  hash_algorithm=hash_algorithm)
+
+        seed = digest  # use digest as next seed
+        del chunks_pool[chunk_index]  # remove chunk from pool
+    return digest  # return last digest
 
 
 def compute_from_path(path, *args, **kwargs):
@@ -50,22 +98,17 @@ def compute_from_path(path, *args, **kwargs):
 
 
 def compute(f, *args, **kwargs):
-    if type(f) in [type(b'bytes'), type('str'), type(u'unicode')]:
+    if _is_filepath(f):
         return compute_from_path(f, *args, **kwargs)
     return compute_from_obj(f, *args, **kwargs)
 
 
-#def sample_from_obj(obj, size, count, seed=b"",
-#                    hash_algorithm=hashlib.sha256):
-#    pass
-#
-#
-#def sample_from_path(path, *args, **kwargs):
-#    with open(path, 'rb') as obj:
-#        return sample_from_obj(obj, *args, **kwargs)
-#
-#
-#def sample(f, *args, **kwargs):
-#    if type(f) in [type(b'bytes'), type('str'), type(u'unicode')]:
-#        return sample_from_path(f, *args, **kwargs)
-#    return sample_from_obj(f, *args, **kwargs)
+def sample_from_path(path, *args, **kwargs):
+    with open(path, 'rb') as obj:
+        return sample_from_obj(obj, *args, **kwargs)
+
+
+def sample(f, *args, **kwargs):
+    if _is_filepath(f):
+        return sample_from_path(f, *args, **kwargs)
+    return sample_from_obj(f, *args, **kwargs)
